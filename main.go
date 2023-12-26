017aa76e1b68b440efc09112d34c1ac5358aa8ee
Jacob Saucelo <jacobsaucelo32@gmail.com>
2023-12-26 10:43:33 +0800

    benchmarking

--- main.go
+++ main.go
@@ -15,7 +15,6 @@ type dataType struct {
 var folderName = "data"
 
 func main() {
-	startNow := time.Now()
 	fpath := filepath.Join(folderName, "data.json")
 
 	data, err := readFile(fpath)
@@ -24,22 +23,42 @@ func main() {
 		return
 	}
 
+	readMono(data)
+	readConcurrently(data)
+
 	// c := make(chan int)
-	d := make(chan []int)
 	// go sum(a[:len(a)/2], c)
 	// go sum(a[len(a)/2:], c)
 	// x, y := <-c, <-c
 	// fmt.Println("x", x, "y", y)
 
-	fpath = filepath.Join(folderName+"/output", "data.json")
+	// fmt.Println("data1", data1, "data2", data2)
+	// saveFile(fpath, data)
+
+}
+
+func readMono(data dataType) {
+	startNow := time.Now()
+	fpath := filepath.Join(folderName+"/output", "data.json")
+
+	saveFile2(fpath, data.Data)
+
+	fmt.Println("Concurrently time: ", time.Since(startNow), " | data:", len(data.Data))
+}
+
+func readConcurrently(data dataType) {
+	startNow := time.Now()
+
+	fpath := filepath.Join(folderName+"/output", "data.json")
+
+	d := make(chan []int)
 	go saveFile(fpath, data.Data[:len(data.Data)/2], d)
 	go saveFile(fpath, data.Data[len(data.Data)/2:], d)
 	data1, data2 := <-d, <-d
 
-	fmt.Println("data1", data1, "data2", data2)
-	fmt.Println("took: ", time.Since(startNow))
-	// saveFile(fpath, data)
-
+	// fmt.Println(data1 + data2)
+	data.Data = append(data1, data2...)
+	fmt.Println("Concurrently time: ", time.Since(startNow), " | data:", len(data.Data))
 }
 
 func sum(a []int, c chan int) {
@@ -73,22 +92,37 @@ func readFile(fpath string) (dataType, error) {
 	return data, nil
 }
 
+func saveFile2(fpath string, data []int) {
+	sum := 0
+
+	for _, res := range data {
+		sum += res
+	}
+	fmt.Println("done")
+}
+
 func saveFile(fpath string, data []int, d chan []int) {
+	sum := 0
+
+	for _, res := range data {
+		sum += res
+	}
+	fmt.Println("done")
 
 	d <- data
-	// saveFile, err := os.Open(fpath)
-	// if err != nil {
-	// 	fmt.Println("error opening save file")
-	// 	return err
-	// }
-	// defer saveFile.Close()
+	// 	saveFile, err := os.Open(fpath)
+	// 	if err != nil {
+	// 		fmt.Println("error opening save file")
+	// 		return err
+	// 	}
+	// 	defer saveFile.Close()
 
-	// encoder := json.NewEncoder(saveFile)
-	// err = encoder.Encode(data)
-	// if err != nil {
-	// 	fmt.Println("error encoding data to json")
-	// 	return err
-	// }
+	// 	encoder := json.NewEncoder(saveFile)
+	// 	err = encoder.Encode(data)
+	// 	if err != nil {
+	// 		fmt.Println("error encoding data to json")
+	// 		return err
+	// 	}
 
-	// return err
+	// 	return err
 }
